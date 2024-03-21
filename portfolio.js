6289d697ee19b7bfdf089461684c94e6c1581227
Fazila2761 <fazilakhan2761@gmail.com>
2024-03-21 11:38:38 +0600

    link complete

--- portfolio.js
+++ portfolio.js
@@ -1,29 +1,29 @@
 
-function projects(){
-    fetch('./portfolio.json')
-    .then(res => res.json())
-    .then(data => displayUsers(data))
-}
-projects();
+// function projects(){
+//     fetch('./portfolio.json')
+//     .then(res => res.json())
+//     .then(data => displayUsers(data))
+// }
+// projects();
 
-function displayUsers(data){
-    const seeDetails = document.getElementById('projects')
- for(const users of data){
-    const div =document.createElement('div');
-    div.innerHTML=`
-    <img src="${users.image}"/>
-    <h3>${users.title}</h3>
-    <p>${users.descreption}</p>
-   <button><a href="${users.github}"><i class="fa-solid fa-code"></i></a></button>
-   <button><a href="${users.netlify}"><i class="fa-solid fa-eye"></i></a></button>
-    `;
-    div.classList.add('projectss')
-    seeDetails.appendChild(div);
+// function displayUsers(data){
+//     const seeDetails = document.getElementById('projects')
+//  for(const users of data){
+//     const div =document.createElement('div');
+//     div.innerHTML=`
+//     <img src="${users.image}"/>
+//     <h3>${users.title}</h3>
+//     <p>${users.descreption}</p>
+//    <button><a href="${users.github}"><i class="fa-solid fa-code"></i></a></button>
+//    <button><a href="${users.netlify}"><i class="fa-solid fa-eye"></i></a></button>
+//     `;
+//     div.classList.add('projectss')
+//     seeDetails.appendChild(div);
  
- console.log(users);
+//  console.log(users);
  
- }
-}
+//  }
+// }
 // ------------------------------form start----------------------------------
 const form = document.querySelector('form');
 const yourName = document.querySelector('#exampleFormControlInput1');
